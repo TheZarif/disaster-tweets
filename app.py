9345094cbee1bbc6ec93f93dd5c5cc535b7c3c3f
TheZarif <zarif.masud@gmail.com>
2023-11-29 15:45:38 -0500

    readme updated

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template, request      
+import joblib
 
 app = Flask(__name__)
 
 class Model:
-    def __init__(self, on):
-        if on:
-            self.on = True
+    def __init__(self, model_type, model_obj):
+        self.model_type = model_type
+        self.model_obj = model_obj
 
     def predict(self, text):
-        return self.on
+        if self.model_type == 'svm':
+            res = self.model_obj.predict([text])
+            return res[0] == 1
 
-def load_model_from_disk():
-    return Model(True)
+def load_model_from_disk(model):
+    if model == 'svm':
+        loaded_pipeline = joblib.load('best_svm_pipeline.pkl')
+        return Model('svm', loaded_pipeline)
 
 # Load your model and store it as a global variable
-model = load_model_from_disk()
+model = load_model_from_disk('svm')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -24,7 +29,7 @@ def index():
     if request.method == 'POST':
         tweet = request.form.get('tweet')
         res = model.predict(tweet)
-        message = f'You submitted the following tweet: "{tweet}" Disaster: {res}'
+        message = f'Disaster: {res}'
 
     return render_template('home.html', tweet=tweet, message=message)
 
